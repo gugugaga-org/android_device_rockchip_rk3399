f156e316271d5b7b2f226653719df3b7e8113972
Zhen Chen <chenzhen@rock-chips.com>
2023-04-14 13:46:01 +0000

    BoardConfig.mk: enable TARGET_RK_GRALLOC_AIDL
    Signed-off-by: Zhen Chen <chenzhen@rock-chips.com>
    Change-Id: Iae23153929dd9d459d01e73acc7fe44dc5f33a7e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -138,6 +138,7 @@ BOARD_HS_ETHERNET := true
 
 # for dynamaic afbc target 
 BOARD_HS_DYNAMIC_AFBC_TARGET := false
+TARGET_RK_GRALLOC_AIDL := true
 TARGET_RK_GRALLOC_VERSION := 4
 
 # Allow deprecated BUILD_ module types to get DDK building
