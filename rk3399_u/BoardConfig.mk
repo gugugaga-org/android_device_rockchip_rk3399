5fa7366846a9192ab24cf469c48cdda6ce3b6cb2
Bian Jin chen <kenjc.bian@rock-chips.com>
2023-07-13 01:22:03 +0000

    Remove recovery fstab for u device.
    Need this CL:
    https://10.10.10.29/c/android/rk/device/rockchip/rksdk/+/191676
    
    Signed-off-by: Bian Jin chen <kenjc.bian@rock-chips.com>
    Change-Id: Ieea1d97a298501dc8d37f9c7f0da32c22fd0b901

--- rk3399_u/BoardConfig.mk
+++ rk3399_u/BoardConfig.mk
@@ -18,5 +18,4 @@ BOARD_HAS_RK_4G_MODEM := false
 
 ifeq ($(strip $(BOARD_USES_AB_IMAGE)), true)
     include device/rockchip/common/BoardConfig_AB.mk
-    TARGET_RECOVERY_FSTAB := device/rockchip/rk3399/rk3399_u/recovery.fstab_AB
 endif
