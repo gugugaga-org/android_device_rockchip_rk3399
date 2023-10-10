83693dfaf96e13c8821d9bfdccf5d9c53000fb65
Wu Liangqing <wlq@rock-chips.com>
2023-10-10 11:45:03 +0000

    Enabled GKI default
    Change-Id: Iaa9aea0cc2f1a2cf2f53bc9a4e464e84a8e6ca77

--- rk3399_u/BoardConfig.mk
+++ rk3399_u/BoardConfig.mk
@@ -1,5 +1,4 @@
-include device/rockchip/rk3399/BoardConfig.mk
-
+BOARD_BUILD_GKI := false
 BOARD_SENSOR_ST := true
 BOARD_SENSOR_COMPASS_AK8963-64 := true
 BOARD_SENSOR_MPU_PAD := false
@@ -11,11 +10,5 @@ BOARD_CAMERA_SUPPORT := true
 BOARD_CAMERA_SUPPORT_EXT := true
 PRODUCT_KERNEL_DTS := rk3399-evb-ind-lpddr4-v13-android-avb
 
-# AB image definition
-BOARD_USES_AB_IMAGE := false
-BOARD_ROCKCHIP_VIRTUAL_AB_ENABLE := false
 BOARD_HAS_RK_4G_MODEM := false
-
-ifeq ($(strip $(BOARD_USES_AB_IMAGE)), true)
-    include device/rockchip/common/BoardConfig_AB.mk
-endif
+include device/rockchip/rk3399/BoardConfig.mk
